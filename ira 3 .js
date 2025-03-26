9fc09fd2f3f33fc11ea65f965cfe297c5e70dbda
ariffiskandar <azrulaiqal7@gmail.com>
2025-03-26 18:42:20 +0800

    Add files via upload

---
+++ ira 3 .js
@@ -0,0 +1,5 @@
+function redirectToSurprise() {
+    // Replace the URL below with the page you want to redirect to
+    window.location.href = "https://azrulaiqal.github.io/sorry-ayang/"; 
+  }
+  
